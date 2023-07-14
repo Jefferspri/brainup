5b59c677f96d3e0a78fba066a570a8ceb83dd758
jeffereson <jefferespri@gmail.com>
2023-07-14 12:45:31 -0500

    performance improve

--- UI_th.py
+++ UI_th.py
@@ -311,14 +311,14 @@ class FourthPage(tk.Frame):
         self.btn_result["state"] = "normal"
         print("Recording Started.")
         
-        #inicio = time.time()
+        inicio = time.time()
         counter = 1
         for i in range(len(images)):
             self.lbl_img.config(image = images[i])
             self.update()
             if i%4 == 0:
-                #print(time.time()- inicio)
-                #inicio = time.time()
+                print(time.time()- inicio)
+                inicio = time.time()
                 t_details["time"].append(datetime.datetime.now()) # transition start time
                 t_details["tag"].append(lst_random_images[counter])
                 counter += 1
@@ -565,7 +565,7 @@ def eeg_writer():
     # Length of the epochs used to compute the FFT (in seconds)
     EPOCH_LENGTH = 1
     # Amount of overlap between two consecutive epochs (in seconds)
-    OVERLAP_LENGTH = 0.8
+    OVERLAP_LENGTH = 0.2
     # Amount to 'shift' the start of each next consecutive epoch
     SHIFT_LENGTH = EPOCH_LENGTH - OVERLAP_LENGTH
         
@@ -588,27 +588,28 @@ def eeg_writer():
     
     fs = int(info.nominal_srate())
     # capture
+    #inicio = time.time()
     while True:
-        #
         try:
             """ 3.1 ACQUIRE DATA """
             # Obtain EEG data from the LSL stream
             if recording:
                 eeg_data, timestamp = inlet.pull_chunk(
                     timeout=1, max_samples=int(SHIFT_LENGTH * fs))
-                print(eeg_data[0])
+                #print(eeg_data[0])
+                #print(time.time() - inicio)
+                #inicio = time.time()
                 all_raw_data['eeg'] = all_raw_data['eeg'] + eeg_data
                 all_raw_data['time'] = all_raw_data['time'] + timestamp
             else:
                 time.sleep(2)
-        #    """     
+        
         except IndexError:
-            # Sleep 10 sec.
+            # Sleep 5 sec.
             time.sleep(5)
-            
             print('Looking for an EEG stream...')
             streams = resolve_byprop('type', 'EEG', timeout=2)
-            print(streams)
+            
             if len(streams) == 0:
                 print('Can\'t find EEG stream.')
             else:
